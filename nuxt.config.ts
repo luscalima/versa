2e45cea9608ba2fc9e9c779c864f164688f1d982
LuscaLima <lusca.lima@outlook.com>
2026-05-31 12:27:42 -0300

    chore: setup test runner and test scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/eslint'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@nuxt/eslint',
+    '@nuxt/test-utils/module'
+  ],
   shadcn: {
     prefix: 'App',
     componentDir: '@/components/ui'
