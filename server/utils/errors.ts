fe124c576d41a1386c200381191cd4987d880ea9
LuscaLima <lusca.lima@outlook.com>
2026-07-22 21:59:44 -0300

    feat: create `methodNotAllowedError`

--- server/utils/errors.ts
+++ server/utils/errors.ts
@@ -20,3 +20,7 @@ function errorOptions(statusCode: number, code: string, opts: AppErrorOptions) {
 export function internalServerError(opts: AppErrorOptions) {
   return createError(errorOptions(500, 'INTERNAL_SERVER_ERROR', opts))
 }
+
+export function methodNotAllowedError(opts: AppErrorOptions) {
+  return createError(errorOptions(405, 'METHOD_NOT_ALLOWED', opts))
+}
