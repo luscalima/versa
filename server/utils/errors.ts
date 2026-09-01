7dd6d1254ebead6adf9786b7ecb71b9c09b2fdb2
LuscaLima <lusca.lima@outlook.com>
2026-08-31 21:46:15 -0300

    feat: add HTTP error helpers

--- server/utils/errors.ts
+++ server/utils/errors.ts
@@ -25,3 +25,15 @@ export function internalServerError(opts: AppErrorOptions) {
 export function methodNotAllowedError(opts: AppErrorOptions) {
   return createError(errorOptions(405, 'METHOD_NOT_ALLOWED', opts))
 }
+
+export function badRequestError(opts: AppErrorOptions) {
+  return createError(errorOptions(400, 'BAD_REQUEST', opts))
+}
+
+export function unprocessableEntityError(opts: AppErrorOptions) {
+  return createError(errorOptions(422, 'UNPROCESSABLE_ENTITY', opts))
+}
+
+export function conflictError(opts: AppErrorOptions) {
+  return createError(errorOptions(409, 'CONFLICT', opts))
+}
