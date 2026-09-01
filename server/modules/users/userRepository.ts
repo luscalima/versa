8d0ed371281cad1b28c44e7421e61d3481b307e9
LuscaLima <lusca.lima@outlook.com>
2026-08-31 21:46:18 -0300

    feat: add user domain model and repository contract

---
+++ server/modules/users/userRepository.ts
@@ -0,0 +1,7 @@
+import type { User } from './user'
+
+export interface UserRepository {
+  findByUsername(username: string): Promise<User | null>
+  findByEmail(email: string): Promise<User | null>
+  save(user: User): Promise<object>
+}
